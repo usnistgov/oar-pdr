02acdc8c6c8896777f3965ff5bccd8fff97df4b1
Ray Plante <raymond.plante@nist.gov>
2017-02-03 12:59:26 -0500

    adding (untested) bagit/prep.py

--- python/nistoar/preserv/exceptions.py
+++ python/nistoar/preserv/exceptions.py
@@ -20,11 +20,83 @@ class PreservationException(Exception):
     """
     a base class for exceptions occuring in the preservation system
     """
-    pass
+    def __init__(msg=None, cause=None):
+        if not msg:
+            if cause:
+                msg = str(cause)
+            else:
+                msg = "Unknown Preservation System Error"
+        super(PreservationException, self).__init__(msg)
+        self.cause = cause
 
 class ConfigurationException(PreservationException):
     """
     a class indicating an error in the configuration of the preservation system
     """
     pass
+
+class StateException(PreservationException):
+    """
+    a class indicating that the preservation system or environment is in 
+    an uncorrectable state preventing proper processing
+    """
+    pass
+
+class SIPDirectoryError(StateException):
+    """
+    a class indicating a problem with the given directory containing 
+    the submission data.
+    """
+    def __init__(self, dir, problem=None, cause=None, msg=None):
+        """
+        initial the exception.  By default the exception message will
+        be formatted by combining the directory name and the problem statement.
+        This can be overridden by providing a verbatim message via the msg
+        parameter.
+
+        :param dir  str:   the directory giving the problem
+        :param problem str:   a statement of what the problem is; this should not
+                           include the name of the directroy.
+        :param cause Exception:  a caught exception that represents the 
+                           underlying cause of the problem.  
+        :param msg  str:   a fully formatted to string to use as the exception
+                           message instead of one formed by combining the 
+                           directory name and its problem.
+        """
+        if not msg:
+            prob = problem
+            if not prob:
+                prob = (cause and str(cause)) or "unspecified problem"
+            msg = "Problem with SIP directory, {0}: {1}".format(dir, prob)
+        super(SIPDirectoryError, self).__init__(msg, cause)
+        self.problem = problem
+                    
+class SIPDirectoryNotFound(SIPDirectoryError):
+    """
+    An exception indicating the SIPDirectory does not exist
+    """
+    def __init__(self, dir, cause=None, msg=None):
+        """
+        :param dir  str:   the directory giving the problem
+        :param cause Exception:  a caught exception that represents the 
+                           underlying cause of the problem.  
+        :param msg  str:   A message to override the default.
+        """
+        prob = "directory not found"
+        super(SIPDirectoryNotFound, self).__init__(dir, prob, cause, msg)
+
+class PODError(StateException):
+    """
+    a problem found in the provided POD data for the dataset
+    """
+    def __init__(self, msg, cause=None, src=None):
+        """
+        :param msg   str:   the error message
+        :param cause Exception:  an optional underlying cause
+        :param src   str:   the name of the file where the data came from
+        """
+        if src:
+            msg += " ({0})".format(src)
+        super(PODError, self).__init__(msg, cause)
+        self.source = src
 
