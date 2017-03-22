06e79e14965b591128080d61d36a7e076351fc93
Ray Plante <raymond.plante@nist.gov>
2017-03-22 10:49:34 -0400

    added install scripts with support for submodule

---
+++ python/setup.py
@@ -0,0 +1,52 @@
+import os, sys, subprocess, pdb
+from distutils.core import setup
+from distutils.command.build import build as _build
+
+def find_oar_metadata(submoddir='oar-metadata'):
+    out = submoddir
+    if not os.path.isabs(out):
+        out = os.path.join(os.path.dirname(
+                           os.path.dirname( os.path.abspath(__file__) )), out)
+    outpy = os.path.join(out, "python", "nistoar")
+    if not os.path.exists(out):
+        msg = "oar-metadata submodule not found in {0} subdirectory" 
+        raise RuntimeError(msg.format(dirname))
+    if not os.path.exists(outpy):
+        msg = "{0} subdirectory does apparently does not contain oar-metadata " \
+              "submodule"
+        raise RuntimeError(msg.format(dirname))
+    return out
+
+def build_oar_metadata(pkgdir, buildlib, buildscrp):
+    pydir = os.path.join(pkgdir, "python")
+    cmd = "python setup.py build --build-lib={0} --build-scripts={1}" \
+        .format(os.path.abspath(buildlib), os.path.abspath(buildscrp)).split()
+    if sys.executable:
+        cmd[0] = sys.executable
+    ex = subprocess.call(cmd, cwd=pydir)
+    if ex != 0:
+        raise RuntimeError("oar-metadata submodule build failed; exit="+str(ex)+
+                           ";\ndir="+pydir+";\ncmd="+" ".join(cmd))
+
+class build(_build):
+
+    def run(self):
+        oarmdpkg = find_oar_metadata()
+        build_oar_metadata(oarmdpkg, self.build_lib, self.build_scripts)
+        _build.run(self)
+
+setup(name='nistoar',
+      version='0.1',
+      description="nistoar/pdr: support for the NIST Public Data Repository",
+      author="Ray Plante",
+      author_email="raymond.plante@nist.gov",
+      url='https://github.com/usnistgov/oar-pdr',
+      scripts=[ '../scripts/ppmdserver.py' ],
+      packages=['nistoar.pdr', 'nistoar.pdr.publish',
+                'nistoar.pdr.publish.mdserv',
+                'nistoar.pdr.preserv', 'nistoar.pdr.preserv.bagger',
+                'nistoar.pdr.preserv.bagit'
+            ],
+      cmdclass={'build': build}
+)
+
---
+++ scripts/install.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+#
+# This script installs the PDR system (including metadata support fronm the
+# oar-metadata submodule.
+#
+set -e
+prog=`basename $0`
+execdir=`dirname $0`
+[ "$execdir" = "" -o "$execdir" = "." ] && execdir=$PWD
+
+base=`(cd $execdir/.. > /dev/null 2>&1; pwd)`
+oarmd_pkg=$base/oar-metadata
+
+[ -d "$oarmd_pkg" -a -d "$oarmd_pkg/python/nistoar" ] || {
+    echo "$prog: Missing oar-metadata submodule"
+    echo Clone the oar-metadata repo in this directory
+    exit 3
+}
+
+. $oarmd_pkg/scripts/_install-env.sh
+
+#install the PDR python library
+mkdir -p $PY_LIBDIR
+echo Installing python libraries into $PY_LIBDIR...
+(cd $SOURCE_DIR/python && python setup.py install --install-purelib=$PY_LIBDIR --install-scripts=$BINDIR)
+
+#install the JAVA jars
+# None at this time
+
+$oarmd_pkg/scripts/install_extras.sh --install-dir=$INSTALL_DIR
+
+
