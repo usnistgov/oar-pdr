ab9fa1d02a8376dbddb17cbf0e5e55deab7e3694
Ray Plante <raymond.plante@nist.gov>
2017-10-27 16:16:29 -0400

    fix json validation testing; required updated schema URIs (and ejsonschema)

--- python/nistoar/pdr/preserv/bagit/validate/nist.py
+++ python/nistoar/pdr/preserv/bagit/validate/nist.py
@@ -1,15 +1,23 @@
 """
 This module implements a validator for the NIST-generated bags
 """
-import os, re
+import os, re, json
 from collections import OrderedDict
 from urlparse import urlparse
+
+import ejsonschema as ejs
 
 from .base import (Validator, ValidatorBase, ALL, ValidationResults,
                    ERROR, WARN, REC, ALL, PROB, AggregatedValidator)
 from .bagit import BagItValidator
 from .multibag import MultibagValidator
 from ..bag import NISTBag
+
+DEF_BASE_NERDM_SCHEMA = "https://data.nist.gov/od/dm/nerdm-schema/v0.1#"
+DEF_NERDM_RESOURCE_SCHEMA = DEF_BASE_NERDM_SCHEMA + "/definitions/Resource"
+DEF_BASE_POD_SCHEMA = "http://data.nist.gov/od/dm/pod-schema/v1.1#"
+DEF_POD_DATASET_SCHEMA = DEF_BASE_POD_SCHEMA + "/definitions/Dataset"
+
 
 class NISTBagValidator(ValidatorBase):
     """
@@ -23,6 +31,14 @@ class NISTBagValidator(ValidatorBase):
     
     def __init__(self, config=None):
         super(NISTBagValidator, self).__init__(config)
+        self.validate = self.cfg.get('validate_metadata', True)
+        self.mdval = None
+        if self.validate:
+            schemadir = self.cfg.get('nerdm_schema_dir')
+            self.mdval = {
+                "_": ejs.ExtValidator.with_schema_dir(schemadir, ejsprefix='_'),
+                "$": ejs.ExtValidator.with_schema_dir(schemadir, ejsprefix='$')
+            }
 
     def test_name(self, bag, want=ALL, results=None):
         out = results
@@ -39,7 +55,233 @@ class NISTBagValidator(ValidatorBase):
             out._warn(t, nm.group(2) == vers[0] and nm.group(3) == vers[1])
 
         return out
+
+    def test_bagit_mdels(self, bag, want=ALL, results=None):
+        out = results
+        if not out:
+            out = ValidationResults(bag.name, want)
+
+        arkstart = "ark:/" + \
+                   self.cfg.get("test_bagit_mdels",{}).get("ark_naan","")
+
+        data = bag.get_baginfo()
+        required = "Source-Organization Contact-Email Bagging-Date Bag-Group-Identifier Internal-Sender-Identifier".split()
+        recommended = "Organization-Address External-Description External-Identifier Bag-Size Payload-Oxum".split()
         
+        i = 0
+        for name in required:
+            i += 1
+            t = self._issue("3-1-"+str(i), "bag-info.txt file must include "+
+                            name)
+            out._err(t, name in data and len(data[name]) > 0 and data[name][-1])
+
+        orgname = "National Institute of Standards and Technology"
+        t = self._issue("3-1-1-1",
+                   "'Source-Organization' must be set to '{0}'".format(orgname))
+        out._err(t, any([orgname in v for v in data.get('Source-Organization',[])]))
+
+        i = 0
+        for name in recommended:
+            i += 1
+            t = self._issue("3-2-"+str(i), "bag-info.txt file must include "+
+                            name)
+            out._rec(t, name in data and len(data[name]) > 0 and data[name][-1])
+
+        orgaddr = "Gaithersburg, MD 20899"
+        t = self._issue("3-2-1-1",
+                   "'Organization-Address' must be set to the NIST address")
+        out._warn(t, any([orgaddr in v
+                                  for v in data.get('Organization-Address',[])]))
+
+        t = self._issue("3-2-3-1",
+              "'External-Identifier' values should include NIST ARK Identifier")
+        out._rec(t, any([v.startswith(arkstart)
+                                  for v in data.get('External-Identifier',[])]))
+
+        return out;
+
+    def test_version(self, bag, want=ALL, results=None):
+        out = results
+        if not out:
+            out = ValidationResults(bag.name, want)
+
+        data = bag.get_baginfo()
+
+        t = self._issue("3-3-1","bag-info.txt must include 'NIST-BagIt-Version'")
+        out._err(t, 'NIST-BagIt-Version' in data)
+
+        val = data.get('NIST-BagIt-Version', [])
+        if t.passed():
+            t = self._issue("3-3-1",
+                            "'NIST-BagIt-Version' value must not be empty")
+            out._err(t, len(val) > 0 and val[-1])
+        if t.passed():
+            t = self._issue("3-3-1",
+                            "'NIST-BagIt-Version' should only be specified once")
+            out._err(t, len(val) == 1)
+        if t.passed():
+            t = self._issue("3-3-1",
+                            "'NIST-BagIt-Version' value must be set to {0}"
+                            .format(self.profile[1]))
+            out._err(t, val[-1] == self.profile[1])
+
+        return out
+        
+    def test_nist_md(self, bag, want=ALL, results=None):
+        out = results
+        if not out:
+            out = ValidationResults(bag.name, want)
+
+        data = bag.get_baginfo()
+
+        self._check_nist_md('3-3-2', 'NIST-POD-Metadata',
+                            "metadata/pod.json", bag, data, out)
+        self._check_nist_md('3-3-3', 'NIST-NERDm-Metadata',
+                            "metadata/nerdm.json", bag, data, out)
+
+        return out
+
+    def _check_nist_md(self, label, elname, expected, bag, data, out):
+        if elname not in data:
+            return out
+        
+        t = self._issue(label,
+                        "'{0}' must not have an empty value".format(elname))
+        out._err(t, len(data[elname]) > 0 and data[elname][-1])
+        if len(data[elname]) == 0:
+            return out
+
+        t = self._issue(label,
+                        "'{0}' must be specified only once".format(elname))
+        out._err(t, len(data[elname]) == 1)
+
+        t = self._issue(label, "'{0}' is typically set to '{1}'"
+                               .format(elname, expected))
+        out._warn(t, data[elname][-1] == expected)
+
+        if data[elname][-1]:
+            t = self._issue(label,
+                            "File given in value of '{0}' must exist as a file"
+                            .format(elname))
+            out._err(t, os.path.isfile(os.path.join(bag.dir,data[elname][-1])))
+
+        return out
+
+    def test_metadata_dir(self, bag, want=ALL, results=None):
+        out = results
+        if not out:
+            out = ValidationResults(bag.name, want)
+
+        t = self._issue("4.1-1",
+                        "Bag must have a tag directory named 'metadata'")
+        out._err(t, os.path.isdir(os.path.join(bag.dir, "metadata")))
+
+        return out
+
+    def test_pod(self, bag, want=ALL, results=None):
+        out = results
+        if not out:
+            out = ValidationResults(bag.name, want)
+        podfile = os.path.join(bag.metadata_dir, "pod.json")
+        
+        t = self._issue("4.1-2-0",
+                        "Metadata tag directory must contain the file, pod.json")
+        out._err(t, os.path.isfile(podfile))
+        if t.failed():
+            return out
+
+        t = self._issue("4.1-2-1",
+                        "pod.json must contain a legal POD Dataset record") 
+        try:
+            with open(podfile) as fd:
+                data = json.load(fd)
+        except Exception as ex:
+            comm = ["Failed reading JSON file: "+str(ex)]
+            out._err(t, False, comm)
+            return out
+        
+        comm = None
+        if self.validate:
+            flav = self._get_mdval_flavor(data)
+            schemauri = data.get(flav+"schema")
+            if not schemauri:
+                schemauri = DEF_POD_DATASET_SCHEMA
+            verrs = self.mdval[flav].validate(data, schemauri=schemauri,
+                                              strict=True, raiseex=False)
+            if verrs:
+                s = (len(verrs) > 1 and "s") or ""
+                comm = ["{0} validation error{1} detected"
+                        .format(len(verrs), s)]
+                comm += [str(e) for e in verrs]
+        out._err(t, not comm, comm)
+            
+        t = self._issue("4.1-3-2", "metadata/pod.json must have "+
+                        "@type=dcat:Dataset")
+        out._err(t, "dcat:Dataset" in data.get("@type",[]))
+        return out
+
+    def _get_mdval_flavor(self, data):
+        """
+        return the prefix (or a default) used to identify meta-properties
+        used for validation.
+        """
+        for prop in "schema extensionSchemas".split():
+            mpfxs = [k[0] for k in data.keys() if k[1:] == prop and k[0] in "_$"]
+            if len(mpfxs) > 0:
+                return mpfxs[0]
+        return "_"
+
+    def test_nerdm(self, bag, want=ALL, results=None):
+        out = results
+        if not out:
+            out = ValidationResults(bag.name, want)
+        mdfile = os.path.join(bag.metadata_dir, "nerdm.json")
+        
+        t = self._issue("4.1-3-0",
+                      "Metadata tag directory must contain the file, nerdm.json")
+        out._err(t, os.path.isfile(mdfile))
+        if t.failed():
+            return out
+
+        t = self._issue("4.1-3-1",
+               "metadata/nerdm.json must contain a legal NERDm Resource record") 
+        try:
+            with open(mdfile) as fd:
+                data = json.load(fd)
+        except Exception as ex:
+            comm = ["Failed reading JSON file: "+str(ex)]
+            out._err(t, False, comm)
+            return out
+
+        comm = None
+        if self.validate:
+            flav = self._get_mdval_flavor(data)
+            schemauri = data.get(flav+"schema")
+            if not schemauri:
+                schemauri = DEF_NERDM_RESOURCE_SCHEMA
+            verrs = self.mdval[flav].validate(data, schemauri=schemauri,
+                                              strict=True, raiseex=False)
+            if verrs:
+                s = (len(verrs) > 1 and "s") or ""
+                comm = ["{0} validation error{1} detected"
+                        .format(len(verrs), s)]
+                comm += [str(e) for e in verrs]
+        out._err(t, not comm, comm)
+            
+        t = self._issue("4.1-3-2", "metadata/nerdm.json must be a NERDm "+
+                        "Resource record")
+        out._err(t, "nrdp:PublicDataResource" in data.get("@type",[]))
+
+        t = self._issue("4.1-3-3",
+               "metadata/nerdm.json must not include any DataFile components")
+        out._err(t, not any(["nrdp:DataFile" in c['@type']
+                             for c in data.get("components",[])]))
+
+        return out
+
+        
+
+
 
 class NISTAIPValidator(AggregatedValidator):
     """
--- python/tests/nistoar/pdr/preserv/bagit/validate/test_nist.py
+++ python/tests/nistoar/pdr/preserv/bagit/validate/test_nist.py
@@ -10,6 +10,10 @@ import nistoar.pdr.preserv.bagit.bag as bag
 import nistoar.pdr.preserv.bagit.exceptions as bagex
 import nistoar.pdr.exceptions as exceptions
 
+pydir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))))))
+basedir = os.path.dirname(pydir)
+schemadir = os.path.join(basedir, "oar-metadata", "model")
+
 datadir = os.path.join( os.path.dirname(os.path.dirname(
                            os.path.dirname(__file__))), "data" )
 bagdir = os.path.join(datadir, "samplembag")
@@ -41,15 +45,19 @@ class TestMultibagValidator(test.TestCase):
         with open(mbtagf, 'w') as fd:
             for line in lines:
                 fd.write(nmre.sub(self.bag.name, line))
+
+        config = {
+            "nerdm_schema_dir": schemadir,
+        }
         
-        self.valid8 = val.NISTBagValidator()
+        self.valid8 = val.NISTBagValidator(config)
 
     def tearDown(self):
         self.tf.clean()
 
     def test_all_test_methods(self):
         expect = ["test_"+m for m in
-          "name".split()]
+          "name bagit_mdels version nist_md metadata_dir pod nerdm".split()]
         expect.sort()
         meths = self.valid8.all_test_methods()
         meths.sort()
@@ -60,7 +68,175 @@ class TestMultibagValidator(test.TestCase):
         self.assertEqual(errs.failed(), [],
                       "False Positives: "+ str([str(e) for e in errs.failed()]))
 
+        badbag = val.NISTBag(self.tf.mkdir("goober"))
+        errs = self.valid8.test_name(badbag)
+        self.assertEqual(len(errs.failed()), 1, "Unexpected # of errors: [\n  " +
+                         "\n  ".join([str(e) for e in errs.failed()]) + "\n]")
+        self.assertTrue(has_error(errs, "2-0"))
         
+        badbag = val.NISTBag(self.tf.mkdir("XXXX.mbag1_0-10"))
+        errs = self.valid8.test_name(badbag)
+        self.assertEqual(len(errs.failed()), 1, "Unexpected # of errors: [\n  " +
+                         "\n  ".join([str(e) for e in errs.failed()]) + "\n]")
+        self.assertTrue(has_error(errs, "2-2"))
+        
+    def test_name_patterns(self):
+        self.assertIsNotNone(self.valid8.namere.match("XXXX.mbag0_2-0"))
+        self.assertIsNotNone(self.valid8.namere.match("XXXX.mbag1_0-10"))
+        self.assertIsNotNone(self.valid8.namere.match("XXXX.mbag13_40-103"))
+
+        self.assertIsNone(self.valid8.namere.match("XXXX.mbag0_2-"))
+        self.assertIsNone(self.valid8.namere.match("XXXX.mbag0.2-3"))
+        self.assertIsNone(self.valid8.namere.match("XXXX.mbag"))
+        self.assertIsNone(self.valid8.namere.match("samplebag"))
+
+    def test_test_bagit_mdels(self):
+        errs = self.valid8.test_bagit_mdels(self.bag)
+        self.assertEqual(errs.failed(), [],
+                      "False Positives: "+ str([str(e) for e in errs.failed()]))
+
+        bif = os.path.join(self.bag.dir, "bag-info.txt")
+        with open(bif) as fd:
+            bilines = fd.readlines()
+
+        with open(bif, 'w') as fd:
+            for line in bilines:
+                if not line.startswith('Source-Organization:') and \
+                   not line.startswith('Organization-Address') and \
+                   "ark:/" not in line:
+                    fd.write(line)
+            print('External-Identifier: doi:10.18434/T45K4', file=fd)
+            print('Organization-Address: 100 Bureau Drive', file=fd)
+
+        errs = self.valid8.test_bagit_mdels(self.bag, True)
+        self.assertEqual(len(errs.failed()), 4, "Unexpected # of errors: [\n  " +
+                         "\n  ".join([str(e) for e in errs.failed()]) + "\n]")
+        self.assertTrue(has_error(errs, "3-1-1"))
+        self.assertTrue(has_error(errs, "3-1-1-1"))
+        self.assertTrue(has_error(errs, "3-2-1-1"))
+        self.assertTrue(has_error(errs, "3-2-3-1"))
+        
+        with open(bif, 'a') as fd:
+            print('External-Identifier: ark:/18434/T45K4', file=fd)
+            print('Source-Organization: NIST', file=fd)
+            print('Organization-Address: Gaithersburg, MD 20899', file=fd)
+            
+        errs = self.valid8.test_bagit_mdels(self.bag, True)
+        self.assertEqual(len(errs.failed()), 1, "Unexpected # of errors: [\n  " +
+                         "\n  ".join([str(e) for e in errs.failed()]) + "\n]")
+        self.assertTrue(has_error(errs, "3-1-1-1"))
+
+    def test_test_version(self):
+        errs = self.valid8.test_version(self.bag)
+        self.assertEqual(errs.failed(), [],
+                      "False Positives: "+ str([str(e) for e in errs.failed()]))
+
+        bif = os.path.join(self.bag.dir, "bag-info.txt")
+        with open(bif) as fd:
+            bilines = fd.readlines()
+
+        with open(bif, 'w') as fd:
+            for line in bilines:
+                if not line.startswith('NIST-BagIt-Version:'):
+                    fd.write(line)
+
+        errs = self.valid8.test_version(self.bag, True)
+        self.assertEqual(len(errs.failed()), 1, "Unexpected # of errors: [\n  " +
+                         "\n  ".join([str(e) for e in errs.failed()]) + "\n]")
+        self.assertTrue(has_error(errs, "3-3-1"))
+        
+    def test_test_nist_md(self):
+        errs = self.valid8.test_nist_md(self.bag)
+        self.assertEqual(errs.failed(), [],
+                      "False Positives: "+ str([str(e) for e in errs.failed()]))
+
+        bif = os.path.join(self.bag.dir, "bag-info.txt")
+        with open(bif, 'a') as fd:
+            print("NIST-POD-Metadata: pod.json", file=fd)
+            print("NIST-NERDm-Metadata: metadata/nerdm.json", file=fd)
+
+        errs = self.valid8.test_nist_md(self.bag)
+        self.assertEqual(len(errs.failed()), 2, "Unexpected # of errors: [\n  " +
+                         "\n  ".join([str(e) for e in errs.failed()]) + "\n]")
+        self.assertTrue(has_error(errs, "3-3-2"))
+        
+        with open(bif, 'a') as fd:
+            print("NIST-NERDm-Metadata: ", file=fd)
+
+        errs = self.valid8.test_nist_md(self.bag)
+        self.assertEqual(len(errs.failed()), 5, "Unexpected # of errors: [\n  " +
+                         "\n  ".join([str(e) for e in errs.failed()]) + "\n]")
+        self.assertTrue(has_error(errs, "3-3-2"))
+        self.assertTrue(has_error(errs, "3-3-3"))
+        
+    def test_test_metadata_dir(self):
+        errs = self.valid8.test_metadata_dir(self.bag)
+        self.assertEqual(errs.failed(), [],
+                      "False Positives: "+ str([str(e) for e in errs.failed()]))
+
+        shutil.rmtree(self.bag.metadata_dir)
+        errs = self.valid8.test_metadata_dir(self.bag)
+        self.assertEqual(len(errs.failed()), 1, "Unexpected # of errors: [\n  " +
+                         "\n  ".join([str(e) for e in errs.failed()]) + "\n]")
+        self.assertTrue(has_error(errs, "4.1-1"))
+                
+    def test_test_pod(self):
+        errs = self.valid8.test_pod(self.bag)
+        self.assertEqual(errs.failed(), [],
+                      "False Positives: "+ str([str(e) for e in errs.failed()]))
+
+        podfile = os.path.join(self.bag.metadata_dir, "pod.json")
+        os.remove(podfile)
+        errs = self.valid8.test_pod(self.bag)
+        self.assertEqual(len(errs.failed()), 1, "Unexpected # of errors: [\n  " +
+                         "\n  ".join([str(e) for e in errs.failed()]) + "\n]")
+        self.assertTrue(has_error(errs, "4.1-2-0"))
+
+        with open(podfile, 'w') as fd:
+            print("Goober!", file=fd)
+        errs = self.valid8.test_pod(self.bag)
+        self.assertEqual(len(errs.failed()), 1, "Unexpected # of errors: [\n  " +
+                         "\n  ".join([str(e) for e in errs.failed()]) + "\n]")
+        self.assertTrue(has_error(errs, "4.1-2-1"))
+
+    def test_get_mdval_flavor(self):
+        data = [
+            {"$schema": 1},
+            {"$extensionSchemas": 1},
+            {"$schema": 1, "$extensionSchemas": 1},
+            {"_schema": 1},
+            {"_extensionSchemas": 1},
+            {"_schema": 1, "_extensionSchemas": 1},
+            {"schema": 1, "extensionSchemas": 1}
+        ]
+        for i in range(len(data)):
+            pfx = self.valid8._get_mdval_flavor(data[i])
+            if i < 3:
+                self.assertEqual(pfx, "$")
+            else:
+                self.assertEqual(pfx, "_")
+
+    def test_test_nerdm(self):
+        errs = self.valid8.test_nerdm(self.bag)
+        self.assertEqual(errs.failed(), [],
+                      "False Positives: "+ str([str(e) for e in errs.failed()]))
+
+        nerdmfile = os.path.join(self.bag.metadata_dir, "nerdm.json")
+        os.remove(nerdmfile)
+        errs = self.valid8.test_nerdm(self.bag)
+        self.assertEqual(len(errs.failed()), 1, "Unexpected # of errors: [\n  " +
+                         "\n  ".join([str(e) for e in errs.failed()]) + "\n]")
+        self.assertTrue(has_error(errs, "4.1-3-0"))
+
+        with open(nerdmfile, 'w') as fd:
+            print("Goober!", file=fd)
+        errs = self.valid8.test_nerdm(self.bag)
+        self.assertEqual(len(errs.failed()), 1, "Unexpected # of errors: [\n  " +
+                         "\n  ".join([str(e) for e in errs.failed()]) + "\n]")
+        self.assertTrue(has_error(errs, "4.1-3-1"))
+
+            
+
 
 if __name__ == '__main__':
     test.main()
