0daccb183073fd947371046a68bc21e0f39c24f8
deoyani <deoyani.pics@gmail.com>
2018-11-29 09:54:55 -0500

    Testing with docker for different ways of reading config values.

--- angular/src/app/shared/config-service/config.service.ts
+++ angular/src/app/shared/config-service/config.service.ts
@@ -17,7 +17,7 @@ export interface Config {
 export class AppConfig {
 private appConfig;
 private confCall;
-private envVariables = "./assets/environment.json";
+private envVariables = "/assets/environment.json";
 private confValues={} as Config;
 
 constructor(private http: HttpClient, @Inject(PLATFORM_ID) 
@@ -25,26 +25,53 @@ constructor(private http: HttpClient, @Inject(PLATFORM_ID)
   
 loadAppConfig() {
     if(isPlatformBrowser(this.platformId)){
+        console.log(" ****** HERE : in browser ::"+this.envVariables);
+        var test =  this.http.get(this.envVariables) 
+        .toPromise()
+        .then(
+            resp =>{
+                resp as Config;
+                console.log(" %%%%%%% TEST 1  in promise then"+ JSON.stringify(resp));
+            },
+            err => {
+                console.log("%%%%%%% ERROR IN CONFIG :"+err);
+            }
+        );
+
+        var test =  this.http.get("https://localhost/pdr/assets/environment.json") 
+        .toPromise()
+        .then(
+            resp =>{
+                resp as Config;
+                console.log(" $$$$$$$ TEST 1  in promise then"+ JSON.stringify(resp));
+            },
+            err => {
+                console.log(" $$$$$$$ ERROR IN CONFIG :"+err);
+            }
+        );
+
         this.confCall =  this.http.get(this.envVariables) 
                         .toPromise()
                         .then(
                             resp =>{
                                 resp as Config;
-                                console.log("TEST 0 in promise then"+ resp);
-                                this.confValues.RMMAPI = process.env.RMMAPI || resp['RMMAPI'];
-                                this.confValues.DISTAPI =  process.env.DISTAPI || resp['DISTAPI'];
-                                this.confValues.LANDING = process.env.LANDING || resp['LANDING'];
-                                this.confValues.METAPI = process.env.METAPI || resp['METAPI'];
-                                this.confValues.SDPAPI = process.env.SDPAPI || resp['SDPAPI'];
-                                this.confValues.PDRAPI =  process.env.PDRAPI || resp['PDRAPI'];
+                                console.log("TEST 0 in promise then"+ JSON.stringify(resp));
+                                this.confValues.RMMAPI =  resp['RMMAPI'];
+                                this.confValues.DISTAPI = resp['DISTAPI'];
+                                this.confValues.LANDING = resp['LANDING'];
+                                this.confValues.METAPI =  resp['METAPI'];
+                                this.confValues.SDPAPI =  resp['SDPAPI'];
+                                this.confValues.PDRAPI =  resp['PDRAPI'];
+                                console.log(" ****** In Browser 1: "+ JSON.stringify(this.confValues));
                             },
                             err => {
                                 console.log("ERROR IN CONFIG :"+err);
                             }
                         );
-        
+                        console.log(" ****** In Browser 2: "+ JSON.stringify(this.confValues));
        return this.confCall; 
     }else{
+        console.log(" ****** HERE : in server");
             
         this.appConfig = <any>data_json;
         this.confValues.RMMAPI = process.env.RMMAPI || this.appConfig.RMMAPI;
@@ -53,10 +80,12 @@ loadAppConfig() {
         this.confValues.METAPI = process.env.METAPI || this.appConfig.METAPI;
         this.confValues.SDPAPI   = process.env.SDPAPI || this.appConfig.SDPAPI;
         this.confValues.PDRAPI =  process.env.PDRAPI || this.appConfig.PDRAPI;
+        console.log(" ****** In server: "+ JSON.stringify(this.confValues));
     }    
 }
 
  getConfig() {
+    console.log(" ****** In Browser 3: "+ JSON.stringify(this.confValues));
     return this.confValues;
  }
 }
