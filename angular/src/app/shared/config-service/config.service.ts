a3f35e286a8059e696206d7bf2c5530535012f4d
deoyani <deoyani.pics@gmail.com>
2018-11-29 12:13:40 -0500

    Updating config service by cleaning up test print statements. Removing process env from browser side env section as they work only for server.

--- angular/src/app/shared/config-service/config.service.ts
+++ angular/src/app/shared/config-service/config.service.ts
@@ -4,6 +4,7 @@ import * as data_json   from '../../../assets/environment.json';
 import * as process from 'process';
 import {  PLATFORM_ID, Inject } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
+import { config } from 'rxjs';
 export interface Config {
     RMMAPI: string ;
     DISTAPI: string;
@@ -18,8 +19,6 @@ export class AppConfig {
 private appConfig;
 private confCall;
 private envVariables = "/assets/environment.json";
-private envVariables2 = "./assets/environment.json";
-
 private confValues={} as Config;
 
 constructor(private http: HttpClient, @Inject(PLATFORM_ID) 
@@ -28,53 +27,26 @@ constructor(private http: HttpClient, @Inject(PLATFORM_ID)
 loadAppConfig() {
     if(isPlatformBrowser(this.platformId)){
         console.log(" ****** HERE : in browser ::"+this.envVariables);
-        var test =  this.http.get(this.envVariables) 
-        .toPromise()
-        .then(
-            resp =>{
-                resp as Config;
-                console.log(" %%%%%%% TEST 1  in promise then"+ JSON.stringify(resp));
-            },
-            err => {
-                console.log("%%%%%%% ERROR IN CONFIG :"+JSON.stringify(err));
-            }
-        );
-
-        var test =  this.http.get("https://localhost/pdr/assets/environment.json") 
-        .toPromise()
-        .then(
-            resp =>{
-                resp as Config;
-                console.log(" $$$$$$$ TEST 1  in promise then"+ JSON.stringify(resp));
-            },
-            err => {
-                console.log(" $$$$$$$ ERROR IN CONFIG :"+JSON.stringify(err));
-            }
-        );
-
-        this.confCall =  this.http.get(this.envVariables2) 
+        this.confCall =  this.http.get(this.envVariables) 
                         .toPromise()
                         .then(
                             resp =>{
                                 resp as Config;
-                                console.log("TEST 0 in promise then"+ JSON.stringify(resp));
                                 this.confValues.RMMAPI =  resp['RMMAPI'];
                                 this.confValues.DISTAPI = resp['DISTAPI'];
                                 this.confValues.LANDING = resp['LANDING'];
                                 this.confValues.METAPI =  resp['METAPI'];
                                 this.confValues.SDPAPI =  resp['SDPAPI'];
-                                this.confValues.PDRAPI =  resp['PDRAPI'];
-                                console.log(" ****** In Browser 1: "+ JSON.stringify(this.confValues));
+                                this.confValues.PDRAPI =   resp['PDRAPI'];
+                                console.log("In Browser read environment variables: "+ JSON.stringify(this.confValues));
                             },
                             err => {
                                 console.log("ERROR IN CONFIG :"+JSON.stringify(err));
                             }
                         );
-                        console.log(" ****** In Browser 2: "+ JSON.stringify(this.confValues));
-       return this.confCall; 
+        return this.confCall; 
     }else{
-        console.log(" ****** HERE : in server");
-            
+        
         this.appConfig = <any>data_json;
         this.confValues.RMMAPI = process.env.RMMAPI || this.appConfig.RMMAPI;
         this.confValues.DISTAPI =  process.env.DISTAPI || this.appConfig.DISTAPI;
@@ -87,7 +59,7 @@ loadAppConfig() {
 }
 
  getConfig() {
-    console.log(" ****** In Browser 3: "+ JSON.stringify(this.confValues));
+    // console.log(" ****** In Browser 3: "+ JSON.stringify(this.confValues));
     return this.confValues;
  }
 }
