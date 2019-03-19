c18794a88becd8e3f0fd588b46c9b86902e31137
deoyani <deoyani.pics@gmail.com>
2019-03-19 14:51:02 -0400

    Reverting back to some of the changes as its not building the application properly.

--- angular/src/environments/environment.prod.ts
+++ angular/src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
   production: true,
+  RMMAPI: 'http://testdata.nist.gov/rmm/',
+  SDPAPI:  'http://testdata.nist.gov/sdp/',
+  PDRAPI:  'http://localhost:5555/#/id/',
+  DISTAPI: 'https://localhost/od/ds/',
+  METAPI:  'http://localhost/metaurl/',
+  LANDING: 'http://testdata.nist.gov/rmm/'
 };
--- angular/src/environments/environment.ts
+++ angular/src/environments/environment.ts
@@ -1,4 +1,11 @@
 
+
 export const environment = {
-  production: true,
+  production: false,
+  RMMAPI: 'http://testdata.nist.gov/rmm/',
+    SDPAPI:  'http://testdata.nist.gov/sdp/',
+    PDRAPI:  'http://localhost:5555/#/id/',
+    DISTAPI: 'https://localhost/od/ds/',
+    METAPI:  'http://localhost/metaurl/',
+    LANDING: 'http://testdata.nist.gov/rmm/'
 };
