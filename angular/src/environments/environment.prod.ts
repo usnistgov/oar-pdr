d78c67809ecd76e3540fc8053406f51491190926
deoyani <deoyani.pics@gmail.com>
2019-03-19 12:47:35 -0400

    Updated files to remove some console errors and messages.

--- angular/src/environments/environment.prod.ts
+++ angular/src/environments/environment.prod.ts
@@ -1,9 +1,3 @@
 export const environment = {
-  production: false,
-  RMMAPI: 'http://testdata.nist.gov/rmm/',
-    SDPAPI:  'http://testdata.nist.gov/sdp/',
-    PDRAPI:  'http://localhost:5555/#/id/',
-    DISTAPI: 'http://testdata.nist.gov/od/ds/',
-    METAPI:  'http://localhost/metaurl/',
-    LANDING: 'http://testdata.nist.gov/rmm/'
+  production: true,
 };
--- angular/src/environments/environment.ts
+++ angular/src/environments/environment.ts
@@ -1,16 +1,4 @@
-// The file contents for the current environment will overwrite these during build.
-// The build system defaults to the dev environment which uses `environment.ts`, but if you do
-// `ng build --env=prod` then `environment.prod.ts` will be used instead.
-// The list of which env maps to which file can be found in `.angular-cli.json`.
-
-// declare var OAR_APP_ENV: any;
 
 export const environment = {
-  production: false,
-  RMMAPI: 'http://testdata.nist.gov/rmm/',
-    SDPAPI:  'http://testdata.nist.gov/sdp/',
-    PDRAPI:  'http://localhost:5555/#/id/',
-    DISTAPI: 'https://localhost/od/ds/',
-    METAPI:  'http://localhost/metaurl/',
-    LANDING: 'http://testdata.nist.gov/rmm/'
+  production: true,
 };
