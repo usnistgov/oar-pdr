6646c4f7fb61c0dcb40b09f8618c6d071e3dac10
deoyani <deoyani.pics@gmail.com>
2019-04-03 13:17:25 -0400

    Updated code for customization API.

---
+++ java/customization-api/src/test/java/gov/nist/oar/custom/updateapi/UpdateapiApplicationTests.java
@@ -0,0 +1,16 @@
+package gov.nist.oar.custom.updateapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UpdateapiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
