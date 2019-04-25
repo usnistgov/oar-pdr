954389d681816b6fd4094044395210b7031cc543
Ray Plante <raymond.plante@nist.gov>
2019-04-25 10:29:52 -0400

    attempt to fix webpack dependency issues

---
+++ angular/webpack.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    node: {
+        crypto: true,
+        fs: true,
+        http: true,
+        path: true,
+        net: true,
+        zlib: true,
+        stream: true
+    }
+}
