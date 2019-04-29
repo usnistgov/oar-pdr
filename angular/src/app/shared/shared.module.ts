594ca5378fd0c9652e67dc20ea75b39a2d8513cb
Ray Plante <raymond.plante@nist.gov>
2019-04-28 22:19:29 -0400

    enable building via build:ssr: deprecate old hea/footbar

--- angular/src/app/shared/shared.module.ts
+++ angular/src/app/shared/shared.module.ts
@@ -3,8 +3,8 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { SearchService } from './search-service/index';
-// import { FootbarComponent } from './footbar/index';
-// import { HeadbarComponent } from './headbar/index';
+import { FootbarComponent as DepFootbarComponent } from './footbar/index';
+import { HeadbarComponent as DepHeadbarComponent } from './headbar/index';
 
 /**
  * Do not specify providers for modules that might be imported by a lazy loaded module.
@@ -12,8 +12,8 @@ import { SearchService } from './search-service/index';
 
 @NgModule({
   imports: [CommonModule, RouterModule],
-/*  declarations: [ HeadbarComponent, FootbarComponent], */
-  exports: [ /* HeadbarComponent, FootbarComponent, */
+  declarations: [ DepHeadbarComponent, DepFootbarComponent], 
+  exports: [ DepHeadbarComponent, DepFootbarComponent, 
     CommonModule, FormsModule, RouterModule]
 })
 export class SharedModule {
