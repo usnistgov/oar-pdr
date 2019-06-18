8390e7588a6c1ca61c67644c1f38715674db1987
deoyani <deoyani.pics@gmail.com>
2019-06-18 12:21:29 -0400

    This subproject is to create sample service provider using SAML. This service provider can connect to NIST saml and local saml identity provider. For the test purpose user can select the identity provider. This is java maven based project using Spring Boot. The https://github.com/spring-projects/spring-security-saml is used to generate the SAML2 extension, dependency from 'develop' branch.

---
+++ java/saml-service-provider/src/main/java/saml/sample/service/serviceprovider/config/AppConfig.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2002-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+*/
+package saml.sample.service.serviceprovider.config;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.saml.provider.SamlServerConfiguration;
+
+@ConfigurationProperties(prefix = "spring.security.saml2")
+@Configuration
+@EntityScan(basePackages = "saml.sample.service.serviceprovider")
+public class AppConfig extends SamlServerConfiguration {
+}
---
+++ java/saml-service-provider/src/main/java/saml/sample/service/serviceprovider/config/SecurityConfiguration.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2002-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+package saml.sample.service.serviceprovider.config;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.saml.provider.service.config.SamlServiceProviderSecurityConfiguration;
+
+import static org.springframework.security.saml.provider.service.config.SamlServiceProviderSecurityDsl.serviceProvider;
+
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+	@Configuration
+	@Order(1)
+	public static class SamlSecurity extends SamlServiceProviderSecurityConfiguration {
+
+		private AppConfig appConfig;
+
+		public SamlSecurity(BeanConfig beanConfig, @Qualifier("appConfig") AppConfig appConfig) {
+			super("/saml/sp/", beanConfig);
+			this.appConfig = appConfig;
+		}
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			super.configure(http);
+			http.apply(serviceProvider())
+				.configure(appConfig);
+		}
+	}
+
+	@Configuration
+	public static class AppSecurity extends WebSecurityConfigurerAdapter {
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http
+				.antMatcher("/**")
+				.authorizeRequests()
+				.antMatchers("/**").authenticated()
+				.and()
+				.formLogin().loginPage("/saml/sp/select")
+			;
+		}
+	}
+
+}
