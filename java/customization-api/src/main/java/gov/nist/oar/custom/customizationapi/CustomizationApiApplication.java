2914beef5cd16035adc087cfc107b3213dbe1b79
deoyani <deoyani.pics@gmail.com>
2019-08-19 13:07:45 -0400

    REmoved auto configuration for mongodb

--- java/customization-api/src/main/java/gov/nist/oar/custom/customizationapi/CustomizationApiApplication.java
+++ java/customization-api/src/main/java/gov/nist/oar/custom/customizationapi/CustomizationApiApplication.java
@@ -2,8 +2,9 @@ package gov.nist.oar.custom.customizationapi;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 //import org.springframework.context.annotation.Bean;
 //import org.springframework.web.servlet.config.annotation.CorsRegistry;
 //import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 //@RefreshScope
 //@ComponentScan(basePackages = {"gov.nist.oar.custom"})
+@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})
 public class CustomizationApiApplication {
     
 //    @Value("${oar.mdserver}")
