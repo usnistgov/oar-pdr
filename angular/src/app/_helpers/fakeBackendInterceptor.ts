50a234e17a3323aa0b92de207390402a17d858af
chuanlin2018 <c.lin@prometheuscomputing.com>
2019-10-15 16:49:36 -0400

    Disable backend mock api

--- angular/src/app/_helpers/fakeBackendInterceptor.ts
+++ angular/src/app/_helpers/fakeBackendInterceptor.ts
@@ -9,112 +9,130 @@ import { ApiToken } from "../shared/auth-service/ApiToken";
 @Injectable()
 export class FakeBackendInterceptor implements HttpInterceptor {
 
-  constructor(private testDataService: TestDataService,
-    private downloadService: DownloadService,
-    private http: HttpClient) { }
+    constructor(private testDataService: TestDataService,
+        private downloadService: DownloadService,
+        private http: HttpClient) { }
 
-  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    // array in local storage for registered users
+    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        // array in local storage for registered users
 
-    const sampleData: any = require('../../assets/sample2.json');
-    const sampleRecord: any = require('../../assets/sampleRecord2.json');
-    const bundlePlanRes: any = require('../../assets/bundle-sample.json');
+        const sampleData: any = require('../../assets/sample2.json');
+        const sampleRecord: any = require('../../assets/sampleRecord2.json');
+        const bundlePlanRes: any = require('../../assets/bundle-sample.json');
 
-    // let httpRequest: any[] = [
-    //     {"url":"https://s3.amazonaws.com/nist-midas/1858/20170213_PowderPlate2_Pad.zip","body":null,"reportProgress":true,"withCredentials":false,"responseType":"blob","method":"GET","headers":{"normalizedNames":{},"lazyUpdate":null,"headers":{}},"params":{"updates":null,"cloneFrom":null,"encoder":{},"map":null},"urlWithParams":"https://s3.amazonaws.com/nist-midas/1858/20170213_PowderPlate2_Pad.zip"}
-    // ];
+        // let httpRequest: any[] = [
+        //     {"url":"https://s3.amazonaws.com/nist-midas/1858/20170213_PowderPlate2_Pad.zip","body":null,"reportProgress":true,"withCredentials":false,"responseType":"blob","method":"GET","headers":{"normalizedNames":{},"lazyUpdate":null,"headers":{}},"params":{"updates":null,"cloneFrom":null,"encoder":{},"map":null},"urlWithParams":"https://s3.amazonaws.com/nist-midas/1858/20170213_PowderPlate2_Pad.zip"}
+        // ];
 
-    // wrap in delayed observable to simulate server api call
-    return of(null).pipe(mergeMap(() => {
-      // get bundlePlan
-      // if (request.url.endsWith('/od/ds/_bundle_plan') && request.method === 'POST') {
-      //   return of(new HttpResponse({ status: 200, body: JSON.parse(bundlePlanRes) }));
-      // }
-      // console.log("request.url", request.url);
-      // For e2e test
-      if (request.url.endsWith('/rmm/records/SAMPLE123456') && request.method === 'GET') {
-        return of(new HttpResponse({ status: 200, body: sampleData }));
-      }
+        // wrap in delayed observable to simulate server api call
+        return of(null).pipe(mergeMap(() => {
+            // get bundlePlan
+            // if (request.url.endsWith('/od/ds/_bundle_plan') && request.method === 'POST') {
+            //   return of(new HttpResponse({ status: 200, body: JSON.parse(bundlePlanRes) }));
+            // }
+            console.log("request.url", request.url);
+            // For e2e test
+            if (request.url.endsWith('/rmm/records/SAMPLE123456') && request.method === 'GET') {
+                return of(new HttpResponse({ status: 200, body: sampleData }));
+            }
 
-      // authenticate
-      if (request.url.endsWith('/saml/login') && request.method === 'GET') {
-        let body: ApiToken = {
-          userId: '1234',
-          token: 'fake-jwt-token'
-        };
-        // window.alert('Click ok to login');
-        return of(new HttpResponse({ status: 200, body }));
-      }
+            // // authenticate
+            // if (request.url.indexOf('auth/_perm/') > -1 && request.method === 'GET') {
+            //     let body: ApiToken = {
+            //         userId: '1234',
+            //         token: 'fake-jwt-token'
+            //     };
+            //     console.log("logging in...")
+            //     return of(new HttpResponse({ status: 200, body }));
+            // }
 
-      if (request.url.endsWith('/auth/token') && request.method === 'GET') {
-        let body: ApiToken = {
-          userId: '1234',
-          token: 'fake-jwt-token'
-        };
-        // window.alert('Click ok to login');
-        return of(new HttpResponse({ status: 200, body }));
-      }
+            // return 401 not authorised if token is null or invalid
+            // if (request.url.indexOf('auth/_perm/') > -1 && request.method === 'GET') {
+            //     let body: ApiToken = {
+            //         userId: '1234',
+            //         token: 'fake-jwt-token'
+            //     };
+            //     console.log("logging in...")
+            //     return Observable.throw(
+            //         JSON.stringify({
+            //             "status": 401,
+            //             "Userid": "xyz@nist.gov",
+            //             "message": "Unauthorizeduser: User token is empty or expired."
+            //         })
 
-      // if (request.url.endsWith('/saml-sp/auth/token') && request.method === 'GET') {
-      //   let body: ApiToken = {
-      //     userId: '1234',
-      //     token: 'fake-jwt-token'
-      //   };
-      //   // window.alert('Click ok to login');
-      //   return of(new HttpResponse({ status: 200, body }));
-      // }
+            //     );
+            // }
 
-      // if (request.url.indexOf('/customization/draft') > -1 && request.method === 'GET') {
-      //   console.log("Interceptor returning sample record...");
-      //   return of(new HttpResponse({ status: 200, body: sampleRecord }));
-      // }
+            // if (request.url.endsWith('/auth/token') && request.method === 'GET') {
+            //     let body: ApiToken = {
+            //         userId: '1234',
+            //         token: 'fake-jwt-token'
+            //     };
+            //     console.log("getting token...")
+            //     // window.alert('Click ok to login');
+            //     return of(new HttpResponse({ status: 200, body }));
+            // }
 
-      // if (request.url.indexOf('/customization/draft') > -1 && request.method === 'PATCH') {
-      //   console.log("Record updated...");
-      //   return of(new HttpResponse({ status: 200, body: undefined }));
-      //   // return Observable.throw('Username or password is incorrect');
-      // }
+            // if (request.url.endsWith('/saml-sp/auth/token') && request.method === 'GET') {
+            //   let body: ApiToken = {
+            //     userId: '1234',
+            //     token: 'fake-jwt-token'
+            //   };
+            //   // window.alert('Click ok to login');
+            //   return of(new HttpResponse({ status: 200, body }));
+            // }
 
-      // if (request.url.indexOf('/customization/draft') > -1 && request.method === 'DELETE') {
-      //   console.log("Record deleted...");
-      //   return of(new HttpResponse({ status: 200, body: undefined }));
-      // }
+            // if (request.url.indexOf('/customization/draft') > -1 && request.method === 'GET') {
+            //   console.log("Interceptor returning sample record...");
+            //   return of(new HttpResponse({ status: 200, body: sampleRecord }));
+            // }
 
-      // if (request.url.indexOf('/customization/savedrec') > -1 && request.method === 'PUT') {
-      //   console.log("Record saved...");
-      //   return of(new HttpResponse({ status: 200, body: undefined }));
-      // }
+            // if (request.url.indexOf('/customization/draft') > -1 && request.method === 'PATCH') {
+            //   console.log("Record updated...");
+            //   return of(new HttpResponse({ status: 200, body: undefined }));
+            //   // return Observable.throw('Username or password is incorrect');
+            // }
 
-      // get bundle
-      // if (request.url.endsWith('/od/ds/_bundle') && request.method === 'POST') {
-      //     // return new Observable(observer => {
-      //     //     observer.next(this.testDataService.getBundle('https://s3.amazonaws.com/nist-midas/1858/20170213_PowderPlate2_Pad.zip', params););
-      //     //     observer.complete();
-      //     //   });
-      //     // return this.testDataService.getBundle('https://s3.amazonaws.com/nist-midas/1858/20170213_PowderPlate2_Pad.zip', bundlePlanRes);
-      //     console.log("Handling /od/ds/_bundle:");
+            // if (request.url.indexOf('/customization/draft') > -1 && request.method === 'DELETE') {
+            //   console.log("Record deleted...");
+            //   return of(new HttpResponse({ status: 200, body: undefined }));
+            // }
 
-      //     const duplicate = request.clone({
-      // method: 'get' 
-      //     })
-      //     return next.handle(request);
-      // }
+            // if (request.url.indexOf('/customization/savedrec') > -1 && request.method === 'PUT') {
+            //   console.log("Record saved...");
+            //   return of(new HttpResponse({ status: 200, body: undefined }));
+            // }
 
-      // pass through any requests not handled above
-      return next.handle(request);
+            // get bundle
+            // if (request.url.endsWith('/od/ds/_bundle') && request.method === 'POST') {
+            //     // return new Observable(observer => {
+            //     //     observer.next(this.testDataService.getBundle('https://s3.amazonaws.com/nist-midas/1858/20170213_PowderPlate2_Pad.zip', params););
+            //     //     observer.complete();
+            //     //   });
+            //     // return this.testDataService.getBundle('https://s3.amazonaws.com/nist-midas/1858/20170213_PowderPlate2_Pad.zip', bundlePlanRes);
+            //     console.log("Handling /od/ds/_bundle:");
 
-    }))
+            //     const duplicate = request.clone({
+            // method: 'get' 
+            //     })
+            //     return next.handle(request);
+            // }
 
-      // call materialize and dematerialize to ensure delay even if an error is thrown (https://github.com/Reactive-Extensions/RxJS/issues/648)
-      .pipe(materialize())
-      .pipe(delay(500))
-      .pipe(dematerialize());
-  }
+            // pass through any requests not handled above
+            return next.handle(request);
+
+        }))
+
+            // call materialize and dematerialize to ensure delay even if an error is thrown (https://github.com/Reactive-Extensions/RxJS/issues/648)
+            .pipe(materialize())
+            .pipe(delay(500))
+            .pipe(dematerialize());
+    }
 }
 
 export let fakeBackendProvider = {
-  // use fake backend in place of Http service for backend-less development
-  provide: HTTP_INTERCEPTORS,
-  useClass: FakeBackendInterceptor,
-  multi: true
+    // use fake backend in place of Http service for backend-less development
+    provide: HTTP_INTERCEPTORS,
+    useClass: FakeBackendInterceptor,
+    multi: true
 };
