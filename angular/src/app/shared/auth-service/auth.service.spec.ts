9bee80f77a651280d2622af9df7f4b952e02f9e3
chuanlin2018 <c.lin@prometheuscomputing.com>
2019-10-16 15:02:18 -0400

    Fixed unit test issue

--- angular/src/app/shared/auth-service/auth.service.spec.ts
+++ angular/src/app/shared/auth-service/auth.service.spec.ts
@@ -4,17 +4,26 @@ import { AuthService } from './auth.service';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { CommonVarService } from '../common-var/common-var.service';
 import { RouterTestingModule } from '@angular/router/testing';
+import { AppConfig } from '../../config/config';
+import { AngularEnvironmentConfigService } from '../../config/config.service';
+import { TransferState } from '@angular/platform-browser';
 
 describe('AuthService', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [HttpClientTestingModule, RouterTestingModule],
-    providers: [
-      {provide: CommonVarService}
-    ]
-  }));
+    let cfg: AppConfig;
+    let plid: Object = "browser";
+    let ts: TransferState = new TransferState();
+    beforeEach(() => {
+        cfg = (new AngularEnvironmentConfigService(plid, ts)).getConfig() as AppConfig;
+        TestBed.configureTestingModule({
+        imports: [HttpClientTestingModule, RouterTestingModule],
+        providers: [
+            { provide: CommonVarService },
+            { provide: AppConfig, useValue: cfg }
+        ]
+    })});
 
-  it('should be created', async(() => {
-    const service: AuthService = TestBed.get(AuthService);
-    expect(service).toBeTruthy();
-  }));
+    it('should be created', async(() => {
+        const service: AuthService = TestBed.get(AuthService);
+        expect(service).toBeTruthy();
+    }));
 });
