9af5544d6218792c8a6390075bb35a73fdbd5e26
Ray Plante <raymond.plante@nist.gov>
2020-12-31 11:34:43 -0500

    make dhsetup.sh smarter about docker versions

--- scripts/dhsetup.sh
+++ scripts/dhsetup.sh
@@ -9,9 +9,19 @@ set -e
 [ -z "$OAR_DOCKERHUB_CRED" ] || {
     DH_USER=`echo $OAR_DOCKERHUB_CRED | sed -e 's/:.*$//'`
     DH_TOKEN=`echo $OAR_DOCKERHUB_CRED | sed -e 's/^[^:]*://'`
-    echo '+' docker login --username '$DH_USER' --password '$DH_TOKEN'
-    docker login --username $DH_USER --password $DH_TOKEN || {
-        echo "dhsetup: docker login failed"
-        false
-    }
+    docker_version=`docker -v | awk '{ print $3}' | sed -e 's/,.*$//'`
+    docker_major_version=`echo $docker_version | sed -e 's/\..*$//'`
+
+    echo '+' docker login --username '$DH_USER'
+    if [ "$docker_major_version" -gt 17 ]; then
+        (echo "$DHTOKEN" | docker login --username $DH_USER --password-stdin) || {
+            echo "dhsetup: docker login failed"
+            false
+        }
+    else
+        docker login --username $DH_USER --password $DH_TOKEN || {
+            echo "dhsetup: docker login failed"
+            false
+        }
+    fi
 }
