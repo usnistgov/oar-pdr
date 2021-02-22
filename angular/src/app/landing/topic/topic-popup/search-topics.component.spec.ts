207466ebcb127ec4631fdda8e322bfcf50a0d6c3
Ray Plante <raymond.plante@nist.gov>
2021-02-22 08:51:09 -0500

    tweak to fix unit tests after cherry-pick:  * add directives module file  * import OverlayPanelModule into TopicModule  * turn off unit test focus to run all tests

--- angular/src/app/landing/topic/topic-popup/search-topics.component.spec.ts
+++ angular/src/app/landing/topic/topic-popup/search-topics.component.spec.ts
@@ -15,7 +15,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { RouterTestingModule } from '@angular/router/testing';
 import { UserMessageService } from '../../../frame/usermessage.service';
 
-fdescribe('SearchTopicsComponent', () => {
+describe('SearchTopicsComponent', () => {
     let component: SearchTopicsComponent;
     let fixture: ComponentFixture<SearchTopicsComponent>;
     let tempTopics: any;
