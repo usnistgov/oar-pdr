3a0e109f1bed0c23f92cb9a43b6186c7945b8c21
Ray Plante <raymond.plante@nist.gov>
2021-03-22 15:16:08 -0400

    datacart: define downloadStatus values as constants, put    status-related coloring into displayprefs.ts

--- angular/src/app/datacart/cartconstants.ts
+++ angular/src/app/datacart/cartconstants.ts
@@ -1,3 +1,7 @@
+/**
+ * This module provide defined constant values used throughout the DataCartModule
+ */
+
 export class CartConstants {
     public static get cartConst(): any { 
         return {
@@ -6,3 +10,40 @@ export class CartConstants {
         }
     }
 }
+
+/**
+ * constants representing the download status of a file in a cart.  
+ */
+export class DownloadStatus {
+
+    /** the last attempt to download the file was successful */
+    static readonly DOWNLOADED = 'downloaded';
+
+    /** 
+     * the process for downloading a set of bundle files is finished.  This applies specifically 
+     * to a collection of bundle (zip) files, rather than any individual file in a cart.
+     */
+    static readonly COMPLETED = 'complete';
+
+    /** the file is currently being downloaded */
+    static readonly DOWNLOADING = 'downloading';
+
+    /** the file is currently being downloaded */
+    static readonly PENDING = 'pending';
+
+    /** a warning was issued while attempting or preparing to download the file */
+    static readonly WARNING = 'warning';
+
+    /** the last attempt to download the file was canceled while it was in progress */
+    static readonly CANCELED = 'canceled';
+
+    /** the last attempt to download the file failed */
+    static readonly FAILED = 'failed';
+
+    /** the last attempt to download the file encountered an error */
+    static readonly ERROR = 'error';
+
+    /** downloading this file has yet to be attempted or queued */
+    static readonly NO_STATUS = '';
+}
+
