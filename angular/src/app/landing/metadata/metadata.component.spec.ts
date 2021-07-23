0484e4ef4099201fb2ffeb82bbba383402a7b052
Ray Plante <raymond.plante@nist.gov>
2021-07-23 15:42:23 -0400

    rehab metadata component for inclusion in Metadata section

--- angular/src/app/landing/metadata/metadata.component.spec.ts
+++ angular/src/app/landing/metadata/metadata.component.spec.ts
@@ -1,31 +1,59 @@
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { MetadataComponent } from './metadata.component';
-import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+
+import { MetadataComponent } from './metadata.component';
+import { MetadataModule } from './metadata.module';
+import { AppConfig } from '../../config/config';
+import { NerdmRes, NERDResource } from '../../nerdm/nerdm';
 import { GoogleAnalyticsService } from '../../shared/ga-service/google-analytics.service';
+import { config, testdata } from '../../../environments/environment';
 
-describe('DescriptionPopupComponent', () => {
-  let component: MetadataComponent;
-  let fixture: ComponentFixture<MetadataComponent>;
+describe('MetadataComponent', () => {
+    let component: MetadataComponent;
+    let fixture: ComponentFixture<MetadataComponent>;
+    let cfg : AppConfig = new AppConfig(config);
+    let rec : NerdmRes = testdata['test1'];
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ MetadataComponent ],
-      imports: [FormsModule],
-      schemas: [NO_ERRORS_SCHEMA],
-      providers: [NgbActiveModal, GoogleAnalyticsService]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [ MetadataModule ],
+            providers: [
+                { provide: AppConfig, useValue: cfg },
+                GoogleAnalyticsService
+            ]
+        }).compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(MetadataComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(MetadataComponent);
+        component = fixture.componentInstance;
+        component.record = rec;
+        component.inBrowser = true;
+        component.ngOnChanges({});
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+
+        let cmpel = fixture.nativeElement;
+        expect(cmpel.querySelector("#metadata-nerdm")).toBeTruthy();
+
+        let el = cmpel.querySelector("p");
+        expect(el).toBeTruthy();
+        el = el.querySelector("a");
+        expect(el).toBeTruthy();
+        expect(el.textContent).toContain("NERDm documentation");
+
+        expect(component.record).toBeTruthy();
+        el = cmpel.querySelector("legend");
+        expect(el).toBeTruthy();
+        expect(el.textContent).toContain("@context");
+    });
+
+    it('getDownloadURL()', () => {
+        expect(component.getDownloadURL()).toEqual("https://data.nist.gov/rmm/records/?@id=ark:/88434/mds0000fbk");
+    });
 });
