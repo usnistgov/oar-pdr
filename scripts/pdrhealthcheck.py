5849e82d74abcb0e4f384ca4d1b7fc5034bfde05
Ray Plante <raymond.plante@nist.gov>
2021-10-31 12:20:35 -0400

    add scripts/pdrhealthcheck.py to drive health checks

---
+++ scripts/pdrhealthcheck.py
@@ -0,0 +1,30 @@
+#! /usr/bin/python
+import sys, os, logging, traceback as tb
+from nistoar.pdr.health import checkcli as cli
+
+prog = os.path.basename(sys.argv[0])
+if prog.endswith('.py'):
+    prog = prog[:-(len('.py'))]
+
+def err(msg):
+    if prog:
+        sys.stderr.write(prog)
+        sys.stderr.write(": ")
+    sys.stderr.write(msg)
+    sys.stderr.write("\n")
+
+try:
+    
+    cli.main(prog, sys.argv[1:])
+    
+except cli.Failure as ex:
+    err(str(ex))
+    sys.exit(ex.exitcode)
+
+except Exception as ex:
+    # unexpected failure
+    tb.print_exc()
+    err(str(ex))
+    sys.exit(1)
+    
+
