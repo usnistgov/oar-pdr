881076ea6fed724ad8106d14d6bcfd593aa66c68
chuanlin2018 <c.lin@prometheuscomputing.com>
2024-04-11 10:30:51 -0400

    Some clean up

--- angular/src/app/landing/tools/menu/menu.component.spec.ts
+++ angular/src/app/landing/tools/menu/menu.component.spec.ts
@@ -1,14 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { AppConfig } from '../../../config/config'
 import { MenuComponent } from './menu.component';
+import { TransferState } from '@angular/platform-browser';
+import { AngularEnvironmentConfigService } from '../../../config/config.service';
+import { testdata } from '../../../../environments/environment';
 
 describe('MenuComponent', () => {
   let component: MenuComponent;
   let fixture: ComponentFixture<MenuComponent>;
 
+  let cfg : AppConfig;
+  let plid : Object = "browser";
+  let ts : TransferState = new TransferState();
+  let md = testdata['test1'];
+
   beforeEach(async () => {
+    cfg = (new AngularEnvironmentConfigService(plid, ts)).getConfig() as AppConfig;
     await TestBed.configureTestingModule({
-      declarations: [ MenuComponent ]
+      declarations: [ MenuComponent ],
+      providers: [
+        { provide: AppConfig,  useValue: cfg }
+    ]
     })
     .compileComponents();
   });
@@ -16,6 +28,7 @@ describe('MenuComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(MenuComponent);
     component = fixture.componentInstance;
+    component.record = md;
     fixture.detectChanges();
   });
 
