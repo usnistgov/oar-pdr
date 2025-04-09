f935112e01975080ba766bf0861fa8f7f8c371e5
Ray Plante <raymond.plante@nist.gov>
2025-04-09 12:58:53 -0400

    bulk-download: make ediid visible in page, fix syntax errors

--- angular/src/app/bulk-download/bulk-download.component.ts
+++ angular/src/app/bulk-download/bulk-download.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
-  selector: 'app-bulk-download',
-  templateUrl: './bulk-download.component.html',
-  styleUrls: ['./bulk-download.component.css']
+    selector: 'app-bulk-download',
+    templateUrl: './bulk-download.component.html',
+    styleUrls: ['./bulk-download.component.css']
 })
 export class BulkDownloadComponent implements OnInit {
+    inBrowser: boolean = false;
+    ediid: string = "dataset-id";
 
-  constructor() { }
+    constructor(private route: ActivatedRoute,
+                @Inject(PLATFORM_ID) private platformId: Object)
+    {
+        this.inBrowser = isPlatformBrowser(platformId);
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        if (this.inBrowser) {
+            this.route.params.subscribe(queryParams => {
+                if (queryParams.id)
+                    this.ediid = queryParams.id;
+            });
+        }
+    }
 
 }
